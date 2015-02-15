842136335042dea4cba78b376dddebc4a33f2108
Luke Whiteley <luarwh@gmail.com>
2015-02-15 17:21:23 +0000

    Created Sessions Controller for logging in and out through the API.

--- web/db/migrate/20150215170552_devise_create_users.rb
+++ web/db/migrate/20150215170552_devise_create_users.rb
@@ -5,6 +5,9 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      ## Token authenticatable
+      t.string :authentication_token
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -36,6 +39,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :authentication_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
