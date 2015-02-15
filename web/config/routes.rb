196413609c3f015bc1ec02b684ac725ed904431d
Luke Whiteley <luarwh@gmail.com>
2015-02-15 19:15:50 +0000

    JSON API now supports account creation.

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   root 'pages#dashboard'
   get 'pages/dashboard'
 
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     devise_scope :user do
+      post 'registrations' => 'registrations#create', :as => 'register'
       post 'sessions' => 'sessions#create', :as => 'login'
       delete 'sessions' => 'sessions#destroy', :as => 'logout'
     end
