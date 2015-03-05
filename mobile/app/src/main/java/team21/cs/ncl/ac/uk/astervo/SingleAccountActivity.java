a98b8ef55b99e23c02fa2a7a4e9f2d64d3e20149
thebillington <b.rebecchi@newcastle.ac.uk>
2015-03-05 00:06:03 +0000

    Added single screen for each account.

--- mobile/app/src/main/java/team21/cs/ncl/ac/uk/astervo/SingleAccountActivity.java
+++ mobile/app/src/main/java/team21/cs/ncl/ac/uk/astervo/SingleAccountActivity.java
@@ -7,51 +7,50 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
 
+public class SingleAccountActivity extends ActionBarActivity {
 
-public class DashActivity extends ActionBarActivity {
-
-    //Get global variables
     Globals g;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dash);
+        setContentView(R.layout.activity_single_account);
 
-        //Update fields every time activity is started
-        update();
+        //Get text fields
+        TextView type = (TextView) findViewById(R.id.singleAccType);
+        TextView bal = (TextView) findViewById(R.id.singleAccBal);
+        TextView interest = (TextView) findViewById(R.id.singleAccInt);
+
+        //Set text fields to that of global single account
+        g = (Globals) getApplication();
+
+        //Get the account details
+        JSONObject thisAcc = g.getSingleAccount();
+
+        try {
+            type.setText("Account Type: " + thisAcc.getString(PrivateFields.TAG_TYPE));
+            bal.setText("Balance: £" + thisAcc.getString(PrivateFields.TAG_BAL));
+            interest.setText("Interest: " + thisAcc.getString(PrivateFields.TAG_INTEREST) + "%");
+        } catch(JSONException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        //Update fields every time activity is resumed
-        update();
-
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_dash, menu);
+        getMenuInflater().inflate(R.menu.menu_single_account, menu);
         return true;
     }
 
@@ -72,7 +71,7 @@ public class DashActivity extends ActionBarActivity {
 
         //Create a progress dialog to show the app logging out
         final ProgressDialog prgDialog;
-        prgDialog = new ProgressDialog(DashActivity.this);
+        prgDialog = new ProgressDialog(SingleAccountActivity.this);
         prgDialog.setMessage("Logging out...");
         prgDialog.setCancelable(false);
         prgDialog.show();
@@ -137,55 +136,4 @@ public class DashActivity extends ActionBarActivity {
         });
 
     }
-
-    public void update() {
-        //Get globals
-        g = (Globals) getApplication();
-
-        //Get bundled extras from previous activity
-        Bundle extras = getIntent().getExtras();
-
-        //Get the Array of accounts
-        final JSONArray jsonAccounts = g.getAccounts();
-
-        //Create a String of type list to store the item Strings
-        List<String> accounts = new ArrayList<String>();
-
-        //Create a for loop to iterate through each account and pull out the relevant data
-        for(int i = 0; i < jsonAccounts.length(); i++) {
-            //Try to get account details from each JSON object
-            try {
-                JSONObject currentAcc = jsonAccounts.getJSONObject(i);
-                String details = "";
-                details += "Account Type: " + currentAcc.getString(PrivateFields.TAG_TYPE) + "\n";
-                details += "Balance: £" + currentAcc.getString(PrivateFields.TAG_BAL) + "\n";
-                details += "Interest Rate: " + currentAcc.getString(PrivateFields.TAG_INTEREST) + "%";
-                accounts.add(details);
-            } catch(JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-        //Create an array adapter to set the List view equal to the information of each account
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(((ListView)findViewById(R.id.accListView)).getContext(), android.R.layout.simple_list_item_1, accounts);
-        ListView displayAcc = (ListView) findViewById(R.id.accListView);
-        displayAcc.setAdapter(adapter);
-
-        //Set on click listener for each item
-        displayAcc.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-
-                try {
-                    g.setSingleAccount(jsonAccounts.getJSONObject(position));
-                    Intent i = new Intent(getApplicationContext(), SingleAccountActivity.class);
-                    startActivity(i);
-                } catch(JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-    }
-
 }
