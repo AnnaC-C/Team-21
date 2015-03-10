3e613f69f9531603e74abe9fc0290bd1c70dc814
Luke Whiteley <luarwh@gmail.com>
2015-03-10 14:00:58 +0000

    Generated Devise views.

---
+++ web/db/migrate/20150306152924_create_quizzes.rb
@@ -0,0 +1,13 @@
+class CreateQuizzes < ActiveRecord::Migration
+  def change
+    create_table :quizzes do |t|
+      t.string :question
+      t.string :correct 
+      t.string :incorrect_1
+      t.string :incorrect_2
+      t.string :incorrect_3
+
+      t.timestamps null: false
+    end
+  end
+end
