3eac952fbbeb0552df2c617d79907cead2c42bdb
thebillington <b.rebecchi@newcastle.ac.uk>
2015-03-11 04:44:11 +0000

    Fixed pet XML. Added missing activities. Added menu bar functionality to all logged in Activities.

--- mobile/app/src/main/java/team21/cs/ncl/ac/uk/astervo/QuizActivity.java
+++ mobile/app/src/main/java/team21/cs/ncl/ac/uk/astervo/QuizActivity.java
@@ -7,59 +7,31 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.ShareActionProvider;
 import android.widget.Toast;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
-public class DashActivity extends ActionBarActivity {
+public class QuizActivity extends ActionBarActivity {
 
-    //Get global variables
     Globals g;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dash);
+        setContentView(R.layout.activity_quiz);
 
-        //Update fields every time activity is started
-        update();
+        g = (Globals) getApplication();
+    }
 
-    }
-    public void buttonOnClick1(View v) {
-        Button button=(Button) v;
-        startActivity(new Intent(getApplication(),TransferActivity.class));
-    }
 
     @Override
-    public void onResume() {
-        super.onResume();
-
-        //Update fields every time activity is resumed
-        update();
-
-    }
-      private ShareActionProvider mShareActionProvider;
-   @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_dash, menu);
-
-       return true;
-
+        return true;
     }
 
     @Override
@@ -110,7 +82,7 @@ public class DashActivity extends ActionBarActivity {
 
         //Create a progress dialog to show the app logging out
         final ProgressDialog prgDialog;
-        prgDialog = new ProgressDialog(DashActivity.this);
+        prgDialog = new ProgressDialog(QuizActivity.this);
         prgDialog.setMessage("Logging out...");
         prgDialog.setCancelable(false);
         prgDialog.show();
@@ -175,56 +147,4 @@ public class DashActivity extends ActionBarActivity {
         });
 
     }
-
-    public void update() {
-        //Get globals
-        g = (Globals) getApplication();
-
-        //Get the Array of accounts
-        final JSONArray jsonAccounts = g.getAccounts();
-
-        //Create a String of type list to store the item Strings
-        List<String> accounts = new ArrayList<String>();
-
-        //Create a for loop to iterate through each account and pull out the relevant data
-        for(int i = 0; i < jsonAccounts.length(); i++) {
-            //Try to get account details from each JSON object
-            try {
-                JSONObject currentAcc = jsonAccounts.getJSONObject(i);
-                String details = "";
-                details += "Account Type: " + currentAcc.getString(PrivateFields.TAG_TYPE) + "\n";
-                details += "Balance: £" + currentAcc.getString(PrivateFields.TAG_BAL) + "\n";
-                details += "Interest Rate: " + currentAcc.getString(PrivateFields.TAG_INTEREST) + "%";
-                accounts.add(details);
-            } catch(JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-        //Create an array adapter to set the List view equal to the information of each account
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(((ListView)findViewById(R.id.accListView)).getContext(), android.R.layout.simple_list_item_1, accounts);
-        ListView displayAcc = (ListView) findViewById(R.id.accListView);
-        displayAcc.setAdapter(adapter);
-
-        //Set on click listener for each item
-        displayAcc.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-
-                g.setSingleAccountLocation(position);
-                Intent i = new Intent(getApplicationContext(), TransferActivity.class);
-                startActivity(i);
-            }
-        });
-
-    }
-
-    public void transfer(View view) {
-
-        g.setSingleAccountLocation(0);
-        Intent i = new Intent(getApplicationContext(), TransferActivity.class);
-        startActivity(i);
-
-    }
-
 }
