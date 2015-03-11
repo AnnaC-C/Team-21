35a92ce075ccf6a92bc83d3f89e661e2f726f1a6
Luke Whiteley <luarwh@gmail.com>
2015-03-11 15:57:47 +0000

    Generate model and controller for Quiz.

--- web/db/migrate/20150311155119_create_quizzes.rb
+++ web/db/migrate/20150311155119_create_quizzes.rb
@@ -2,7 +2,7 @@ class CreateQuizzes < ActiveRecord::Migration
   def change
     create_table :quizzes do |t|
       t.string :question
-      t.string :correct 
+      t.string :correct
       t.string :incorrect_1
       t.string :incorrect_2
       t.string :incorrect_3
