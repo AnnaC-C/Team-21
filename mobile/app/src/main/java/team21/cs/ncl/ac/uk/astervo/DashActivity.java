ffb75d4947fc2836d390715e10194b1896ff010e
Khalig <x.babayev@gmail.com>
2015-03-12 01:32:40 +0000

    final-design-of home-page

--- mobile/app/src/main/java/team21/cs/ncl/ac/uk/astervo/DashActivity.java
+++ mobile/app/src/main/java/team21/cs/ncl/ac/uk/astervo/DashActivity.java
@@ -7,28 +7,17 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.ShareActionProvider;
 import android.widget.Toast;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 
 public class DashActivity extends BaseActivity {
 
-    //Get global variables
     Globals g;
 
     long now;
@@ -38,27 +27,15 @@ public class DashActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash);
 
-        //Update fields every time activity is started
-        update();
+        g = (Globals) getApplication();
+    }
 
-    }
-    public void buttonOnClick1(View v) {
-        Button button=(Button) v;
-        startActivity(new Intent(getApplication(),TransferActivity.class));
-    }
 
     @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-   @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_dash, menu);
-
-       return true;
-
+        return true;
     }
 
     @Override
@@ -210,56 +187,4 @@ public class DashActivity extends BaseActivity {
         });
 
     }
-
-    public void update() {
-        //Get globals
-        g = (Globals) getApplication();
-
-        //Get the Array of accounts
-        final JSONArray jsonAccounts = g.getAccounts();
-
-        //Create a String of type list to store the item Strings
-        List<String> accounts = new ArrayList<String>();
-
-        //Create a for loop to iterate through each account and pull out the relevant data
-        for(int i = 0; i < jsonAccounts.length(); i++) {
-            //Try to get account details from each JSON object
-            try {
-                JSONObject currentAcc = jsonAccounts.getJSONObject(i);
-                String details = "";
-                details += "Account Type: " + currentAcc.getString(PrivateFields.TAG_TYPE) + "\n";
-                details += "Balance: £" + currentAcc.getString(PrivateFields.TAG_BAL) + "\n";
-                details += "Interest Rate: " + currentAcc.getString(PrivateFields.TAG_INTEREST) + "%";
-                accounts.add(details);
-            } catch(JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-        //Create an array adapter to set the List view equal to the information of each account
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(((ListView)findViewById(R.id.accListView)).getContext(), android.R.layout.simple_list_item_1, accounts);
-        ListView displayAcc = (ListView) findViewById(R.id.accListView);
-        displayAcc.setAdapter(adapter);
-
-        //Set on click listener for each item
-        displayAcc.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-
-                g.setSingleAccountLocation(position);
-                Intent i = new Intent(getApplicationContext(), TransferActivity.class);
-                startActivity(i);
-            }
-        });
-
-    }
-
-    public void transfer(View view) {
-
-        g.setSingleAccountLocation(0);
-        Intent i = new Intent(getApplicationContext(), TransferActivity.class);
-        startActivity(i);
-
-    }
-
 }
