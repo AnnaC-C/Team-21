ac591cd8aef3f21439c5456ae92189171ba635c4
Luke Whiteley <luarwh@gmail.com>
2015-03-13 11:52:00 +0000

    Changed format of questions returned from the API.

--- web/app/helpers/quiz_helper.rb
+++ web/app/helpers/quiz_helper.rb
@@ -3,10 +3,7 @@ module QuizHelper
   def retrieve_random_questions
     return Question.all.map{
       |x| { :question => x.question,
-            :a => x.correct,
-            :b =>  x.incorrect_1,
-            :c =>  x.incorrect_2,
-            :d =>  x.incorrect_3
+            :answers => [x.correct, x.incorrect_1, x.incorrect_2, x.incorrect_3]
           }
       }.sample(5)
   end
