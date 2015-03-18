719a26dea6f16a67d57af9d2c9e789482b266122
Luke Whiteley <luarwh@gmail.com>
2015-03-18 16:40:05 +0000

    Made a start on the website's quiz.

--- web/app/controllers/quiz_controller.rb
+++ web/app/controllers/quiz_controller.rb
@@ -7,8 +7,10 @@ class QuizController < ApplicationController
 
   def get_score
     #calculate_score(params[:answers])
+    answers = []
     5.times do |i|
       logger.info "param:" + params[:"#{i+1}"].to_s
+      answers.push { :question => params}
     end
     redirect_to :root
   end
